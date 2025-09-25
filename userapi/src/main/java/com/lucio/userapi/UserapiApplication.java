100e6b21e8ff005a826de8f320308c9f8618f24a
lcostaju <luciocj_eng@outlook.com>
2025-09-25 20:00:04 -0300

    Enable MongoDB repositories in UserapiApplication

--- userapi/src/main/java/com/lucio/userapi/UserapiApplication.java
+++ userapi/src/main/java/com/lucio/userapi/UserapiApplication.java
@@ -2,8 +2,10 @@ package com.lucio.userapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class UserapiApplication {
 
 	public static void main(String[] args) {
